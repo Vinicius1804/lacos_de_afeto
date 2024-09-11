4c8fc8c3b470a4fae2f2019f6acd16cedb0d559c
JeffersonJesusDev <jefferson3680@hotmail.com>
2024-09-11 17:43:28 -0300

    Implementando JavaScript

--- Nubibus Home-Page/javascriptb.js
+++ Nubibus Home-Page/javascriptb.js
@@ -1,3 +1,22 @@
-function secao2(){
+window.addEventListener('scroll', function() {
+    const nuvem = document.querySelector('.nuvem img');
+    let scrollPos = window.scrollY;
     
+    // A posição da imagem vai subir conforme o scroll - agora movemos para cima
+    nuvem.style.transform = 'translateY(' + scrollPos * -0.3 + 'px)';
+});
+
+function show_camada1(){
+    document.getElementById("camada1").style.visibility="visible";
+    document.getElementById("camada2").style.visibility="hidden";
+}
+
+function show_camada2(){
+    document.getElementById("camada1").style.visibility="hidden";
+    document.getElementById("camada2").style.visibility="visible";
+}
+
+function some_tudo(){
+    document.getElementById("camada1").style.visibility="hidden";
+    document.getElementById("camada2").style.visibility="hidden";
 }
