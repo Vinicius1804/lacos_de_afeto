e4d0f636b59ed76a50a64dbcd3912b4101716fce
JeffersonJesusDev <jefferson3680@hotmail.com>
2024-09-12 01:39:58 -0300

    atualizando o código e adicionando mais páginas

---
+++ Nubibus Home-Page/Apoie/apoie.js
@@ -0,0 +1,11 @@
+function show_camada1(){
+    document.getElementById("camada1").style.visibility="visible";
+}
+
+function show_camada2(){
+    document.getElementById("camada1").style.visibility="hidden";
+}
+
+function some_tudo(){
+    document.getElementById("camada1").style.visibility="hidden";
+}
